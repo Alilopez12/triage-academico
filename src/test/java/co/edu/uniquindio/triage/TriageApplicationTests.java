133dd85e55b5aeca39509c4723bd585937635453
Alilopez12 <allisonlopez1928@gmail.com>
2026-03-01 14:39:31 -0500

    feat: proyecto base Spring Boot

---
+++ src/test/java/co/edu/uniquindio/triage/TriageApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.triage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
