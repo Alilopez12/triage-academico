b0bf330c3f05ecb7a7f6ad9ff3ff86d5035781dc
Juan David Castañeda <juance724@gmail.com>
2026-04-18 10:29:09 -0500

    correccion de errores

---
+++ src/main/java/co/edu/uniquindio/triage/dto/response/SugerenciaClasificacionResponse.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.triage.dto.response;
+
+public class SugerenciaClasificacionResponse {
+}
---
+++ src/test/java/co/edu/uniquindio/triage/SolicitudTest.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.triage;
+
+public class SolicitudTest {
+}
